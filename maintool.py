f4f929180ed725d5b895c38233bfa7dfca0ebee2
kalalannn <xvorob00@stud.fit.vutbr.cz>
2019-02-05 17:30:39 +0100

    1 Design Version.1

--- maintool.py
+++ maintool.py
@@ -1,27 +1,5 @@
 from tkinter import *
-
-ROOT_WIDTH=700
-ROOT_HEIGHT=600
-
-TOP_WIDTH=700
-TOP_HEIGHT=100
-
-BUTTON_FRAME_WIDTH=460
-BUTTON_FRAME_HEIGHT  = ROOT_HEIGHT - TOP_HEIGHT
-
-HISTORY_FRAME_WIDTH  = ROOT_WIDTH - BUTTON_FRAME_WIDTH
-HISTORY_FRAME_HEIGHT = BUTTON_FRAME_HEIGHT
-
-BUTTON_WIDTH  = 6
-BUTTON_HEIGHT = 6
-
-SIMPLE_BUTTONS = [
-        'C', '/', 'X',   '<-',
-        '7', '8', '9',   '*',
-        '4', '5', '6',   '-',
-        '1', '2', '3',   '+',
-        '0', '.', '+/-', '='
-        ]
+from maintool_variables import * # Width and Height
 
 root = Tk()
 root.title('Calculator')
