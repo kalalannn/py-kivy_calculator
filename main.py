f6ec4b3c89ee2b8bd367760ff8dcfdb0f0b7249a
kalalannn <xvorob00@stud.fit.vutbr.cz>
2019-03-14 15:01:16 +0100

    Menu + History + Input

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ from kivy.uix.widget import Widget
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 
 #! Window.width = 500
 Config.set('graphics', 'width', '500')
@@ -34,6 +35,31 @@ class OurSpecialButton(Button):
         super(OurSpecialButton, self).__init__(**kwargs)
 
 """
+    
+"""
+class InputWidget(TextInput):
+    def __init__(self, **kwargs):
+        super(InputWidget, self).__init__(**kwargs)
+
+
+"""
+"""
+class MenuWidget(BoxLayout):
+    oritentation = 'horizontal'
+    color = 'red'
+    def __init__(self, **kwargs):
+        super(MenuWidget, self).__init__(**kwargs)
+        self.add_widget(Button(text='this will be a menu'))
+
+"""
+"""
+class HistoryWidget(BoxLayout):
+    oritentation = 'vertical'
+    def __init__(self, **kwargs):
+        super(HistoryWidget, self).__init__(**kwargs)
+        self.add_widget(Button(text='this will be a history'))
+
+"""
     Fixed input buttons
 """
 class InputButtonWidget(GridLayout):
@@ -50,6 +76,15 @@ class InputButtonWidget(GridLayout):
         super(InputButtonWidget, self).__init__(**kwargs)
         for x in self.array:
             self.add_widget(OurSpecialButton(text=x));
+"""
+"""
+class HistoryAndButtons(BoxLayout):
+    orientaition = 'horizontal'
+    def __init__(self, **kwargs):
+        super(HistoryAndButtons, self).__init__(**kwargs)
+        self.add_widget(InputButtonWidget())
+        self.add_widget(HistoryWidget())
+
 
 """
     RootWidget class is a parent of all Widgets
@@ -58,7 +93,9 @@ class RootWidget(BoxLayout):
     orientation = "vertical"
     def __init__(self, **kwargs):
         super(RootWidget, self).__init__(**kwargs)
-        self.add_widget(InputButtonWidget())
+        self.add_widget(MenuWidget(size_hint=(1, .2)))
+        self.add_widget(InputWidget(size_hint=(1, .2)))
+        self.add_widget(HistoryAndButtons())
 
 """
     Main App class
